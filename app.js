365f6f8d2405e9ec68ca0927ea19abce11727460
Ali Ammaar <alie.ammaar@gmail.com>
2020-01-22 15:56:41 +0500

    ngMessages

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngMessages', 'ngResources']);
 
 myApp.controller('mainController', function($scope, $log, $filter) {
 
