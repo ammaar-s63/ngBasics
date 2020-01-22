c0cfc8983afa3151107f0778bc671f46ca5f28e2
Ali Ammaar <alie.ammaar@gmail.com>
2020-01-22 18:37:01 +0500

    AngularJS Digest

--- app.js
+++ app.js
@@ -9,4 +9,19 @@ myApp.controller('mainController', ['$scope', '$filter', function($scope, $filte
 
   };
 
+$scope.$watch('name', function(newValue, oldValue) {
+
+    console.info('Changed!');
+    console.log('Old:' + oldValue);
+    console.log('New:' + newValue);
+
+  });
+
+  setTimeout(function() {
+    $scope.$apply(function() {
+      $scope.name = "Ali Ammaar"
+      console.log('Scope Changed!')
+    });
+  }, 3000);
+
 }]);
