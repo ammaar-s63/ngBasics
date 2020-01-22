98da40ae7a916b2e1a8f504ca49d21264b61a75c
Ali Ammaar <alie.ammaar@gmail.com>
2020-01-22 17:15:06 +0500

    timeout

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
-myApp.controller('mainController', ['$scope', '$log', '$filter', function($scope, $log, $filter) {
+myApp.controller('mainController', ['$scope', '$timeout', function($scope, $timeout) {
   $scope.name = 'Ammaar'
+  $timeout(function () {
+
+  $scope.name = 'World'
+
+}, 3000);
 }]);
