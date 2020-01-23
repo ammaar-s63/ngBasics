7b9cccaf29215b23dbf70c06a9f7b976d178813f
Ali Ammaar <alie.ammaar@gmail.com>
2020-01-23 18:29:27 +0500

    ng-cloak

--- app.js
+++ app.js
@@ -2,17 +2,10 @@ var myApp = angular.module('myApp', ['ngResource']);
 
 myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
 
-  $scope.name = "";
-  $scope.lowercaseName = function() {
-    return $filter('lowercase')($scope.name)
+  $scope.alertClick  = function(){
+    alert("Button Clicked !");
   };
 
-  $scope.characters = 5;
-
-  $scope.rules = [
-    { rulename: "Must be characters only." },
-    { rulename: "Must be Unique."},
-    { rulename: "Must be awesome!"}
-  ]
+  $scope.name = "Rana Ali"
 
 }]);
