a942651e1e32df5b49076bf4efa9e0ef42714273
Rana Ali Ammaar <alie.ammaar@gmail.com>
2020-01-27 20:05:59 +0500

    Create and enslist rules from rails API

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
 var myApp = angular.module('myApp', ['ngResource']);
 
-myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
+myApp.controller('mainController', ['$scope', '$filter', '$http', function($scope, $filter, $http) {
 
-  $scope.name = "";
+  $scope.name = '';
   $scope.lowercaseName = function() {
     return $filter('lowercase')($scope.name)
   };
 
   $scope.characters = 5;
 
- let rulesrequest = new XMLHttpRequest();
- rulesrequest.onreadystatechange = function () {
-  $scope.$apply(function () {
-    if (rulesrequest.readyState == 4 && rulesrequest.status == 200 )
-      $scope.rules = JSON.parse(rulesrequest.responseText)
-  });
- }
+  $http.get('http://localhost:3000/rules.json')
+    .success(function (result) {
 
- rulesrequest.open("GET", "http://localhost:3000/rules.json", true);
- rulesrequest.send();
+      $scope.rules = result;
 
+    })
+
+    .error(function (data, status) {
+
+      console.log(data);
+
+    })
+
+  $scope.newRule = '';
+
+  $scope.addRule = function () {
+    $http.post('http://localhost:3000/rules', { name: $scope.newRule } )
+      .success(function (result) {
+
+        $scope.rules = result;
+        $scope.newRule = '';
+
+      })
+
+      .error(function (data, status) {
+
+        console.log(data);
+
+      })
+  }
 
 }]);
