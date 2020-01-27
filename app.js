47c14c87908d1555f03938847636abf19b8fe6d7
Rana Ali Ammaar <alie.ammaar@gmail.com>
2020-01-27 14:41:44 +0500

    grab rules from external API

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ var myApp = angular.module('myApp', ['ngResource']);
 
 myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
 
-  $scope.alertClick  = function(){
-    alert("Button Clicked !");
+  $scope.name = "";
+  $scope.lowercaseName = function() {
+    return $filter('lowercase')($scope.name)
   };
 
-  $scope.name = "Rana Ali"
+  $scope.characters = 5;
+
+ let rulesrequest = new XMLHttpRequest();
+ rulesrequest.onreadystatechange = function () {
+  $scope.$apply(function () {
+    if (rulesrequest.readyState == 4 && rulesrequest.status == 200 )
+      $scope.rules = JSON.parse(rulesrequest.responseText)
+  });
+ }
+
+ rulesrequest.open("GET", "http://localhost:3000/rules.json", true);
+ rulesrequest.send();
+
 
 }]);
