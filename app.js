2448225b1b294de5659bc82d661b8adc1efdc469
Rana Ali Ammaar <alie.ammaar@gmail.com>
2020-01-28 13:14:52 +0500

    Resolved: conflicts with master branch

--- app.js
+++ app.js
@@ -1,43 +1,10 @@
-var myApp = angular.module('myApp', ['ngResource']);
+window.addEventListener('hashchange', function () {
 
-myApp.controller('mainController', ['$scope', '$filter', '$http', function($scope, $filter, $http) {
 
-  $scope.name = '';
-  $scope.lowercaseName = function() {
-    return $filter('lowercase')($scope.name)
-  };
+  console.log('Hash Chnaged : ' + window.location.hash)
 
-  $scope.characters = 5;
-
-  $http.get('http://localhost:3000/rules.json')
-    .success(function (result) {
-
-      $scope.rules = result;
-
-    })
-
-    .error(function (data, status) {
-
-      console.log(data);
-
-    })
-
-  $scope.newRule = '';
-
-  $scope.addRule = function () {
-    $http.post('http://localhost:3000/rules', { name: $scope.newRule } )
-      .success(function (result) {
-
-        $scope.rules = result;
-        $scope.newRule = '';
-
-      })
-
-      .error(function (data, status) {
-
-        console.log(data);
-
-      })
+  if (window.location.hash === '#bookmark') {
+    console.log('bookmark is selected.');
   }
 
-}]);
+});
