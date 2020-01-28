387fe6695288348c32eb50e67a4babe804e42960
Rana Ali Ammaar <alie.ammaar@gmail.com>
2020-01-28 12:40:06 +0500

    Feat: multiple controller

--- app.js
+++ app.js
@@ -1,43 +1,13 @@
 var myApp = angular.module('myApp', ['ngResource']);
 
-myApp.controller('mainController', ['$scope', '$filter', '$http', function($scope, $filter, $http) {
+myApp.controller('mainController', ['$scope', function($scope) {
 
-  $scope.name = '';
-  $scope.lowercaseName = function() {
-    return $filter('lowercase')($scope.name)
-  };
+  $scope.name = 'Main Controller';
 
-  $scope.characters = 5;
+}]);
 
-  $http.get('http://localhost:3000/rules.json')
-    .success(function (result) {
+myApp.controller('secondController', ['$scope', function($scope) {
 
-      $scope.rules = result;
-
-    })
-
-    .error(function (data, status) {
-
-      console.log(data);
-
-    })
-
-  $scope.newRule = '';
-
-  $scope.addRule = function () {
-    $http.post('http://localhost:3000/rules', { name: $scope.newRule } )
-      .success(function (result) {
-
-        $scope.rules = result;
-        $scope.newRule = '';
-
-      })
-
-      .error(function (data, status) {
-
-        console.log(data);
-
-      })
-  }
+  $scope.name = 'Second Controller';
 
 }]);
